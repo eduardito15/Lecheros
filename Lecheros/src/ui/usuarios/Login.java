4cbd8bfa06821d22c6fb283f802bb48b33f5465d
Eduardo Vecino <eduardovecino15@gmail.com>
2017-07-26 23:50:21 -0300

    cambio de etiqueta de version.

--- Lecheros/src/ui/usuarios/Login.java
+++ Lecheros/src/ui/usuarios/Login.java
@@ -25,7 +25,7 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
-        jLabelVersion.setText(Lecheros.nombreEmpresa + " Versión 3.4");
+        jLabelVersion.setText(Lecheros.nombreEmpresa + " Versión 3.5");
         jLabelEspera.setVisible(false);
         jTextFieldNombre.requestFocus();
         jTextFieldNombre.selectAll();
